0eccf636102f290b42196c065cc9d1bcf70a7840
BreonDay <bday001@odu.edu>
2017-04-17 22:58:23 -0400

    Add files via upload

---
+++ Submissions/A8/Src/Question6/simasciidendo.py
@@ -0,0 +1,4 @@
+import clusters 
+blognames,words,data=clusters.readfile('blogdata2.txt') # returns blog titles, words in blog (10%-50% boundaries), list of frequency info
+clust=clusters.hcluster(data) # returns a tree of foo.id, foo.left, foo.right
+clusters.printclust(clust,labels=blognames) # walks tree and prints ascii approximation of a dendogram; distance measure is Pearson's r
